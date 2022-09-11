a105af8e286e2dad4a260b0050ab7bbe932aa58b
musman25831 <usman232429@gmail.com>
2022-09-11 22:12:06 +0500

    Custom Counter

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -1,10 +1,72 @@
-import styles from "./Slider.module.scss"
+import { useEffect, useState } from "react";
+import styles from "./Slider.module.scss";
 
-const Slider =()=> {
+const Slider = () => {
+  const [counter, setCounter] = useState(1);
+
+ 
+  const handleIncrement = () => {
+    if (counter <= 4) {
+      setCounter(counter + 1);
+    }
+  };
+  const handleDeccrement = () => {
+    if (counter >= 2) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div className={styles.main}>
-      <h1>HELLO WORLD</h1>
+      <div className={styles.mainList}>
+        {counter === 1 ? (
+          <>
+            <h1 className={styles.heading}>0{counter}</h1>
+            <h2 className={styles.tile}>New Leagcy</h2>
+            <p className={styles.wrap}>Lorem Lorem Lorem% Lorem% </p>
+          </>
+        ) : counter === 2 ? (
+          <>
+            <h1>0{counter}</h1>
+            <h2>New Partial</h2>
+            <p className={styles.wrap}>Lorem Lorem Lorem% Lorem% </p>
+
+          </>
+        ) : counter === 3 ? (
+          <>
+            <h1>0{counter}</h1>
+            <h2>New Item</h2>
+            <p className={styles.wrap}>Lorem Lorem Lorem% Lorem% </p>
+
+          </>
+        ) : counter === 4 ? (
+          <>
+            <h1>0{counter}</h1>
+            <h2>New Professional</h2>
+            <p className={styles.wrap}>Lorem Lorem Lorem% Lorem% </p>
+
+          </>
+        ) : (
+          <>
+            <h1>0{counter}</h1>
+            <h2>New Customs</h2>
+            <p className={styles.wrap}>Lorem Lorem Lorem% Lorem% </p>
+
+          </>
+        )}
+      </div>
+      <div className={styles.innerList}>
+        <span onClick={handleDeccrement} className={styles.incre}>
+          &lt;
+        </span>
+        <span className={styles.conuterSteps}>
+          0{counter}/05
+        </span>
+        <span onClick={handleIncrement} className={styles.incre}>
+          &gt;
+        </span>
+      </div>
     </div>
-  )
-}
-export default  Slider;
+  );
+};
+export default Slider;
