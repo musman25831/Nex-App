aab99cad5ef2afb44e6e1d92e4b8176505bb4e24
musman25831 <usman232429@gmail.com>
2022-09-11 19:36:22 +0500

    slider set ready

---
+++ components/Slider/Slider.js
@@ -0,0 +1,10 @@
+import styles from "./Slider.module.scss"
+
+const Slider =()=> {
+  return (
+    <div className={styles.main}>
+      <h1>HELLO WORLD</h1>
+    </div>
+  )
+}
+export default  Slider;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Slider from '../components/slider/slider'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>Helooo</h1>
+      <Slider/>
     </div>
   )
 }
