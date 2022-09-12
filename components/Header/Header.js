cf804c4832ad0260912db35816ce88452b24e109
musman25831 <usman232429@gmail.com>
2022-09-13 01:57:03 +0500

    set up header file

---
+++ components/Header/Header.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styles from "./Header.module.scss"
+
+const Header = ()=>{
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Header;
